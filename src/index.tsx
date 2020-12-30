30173d1cad76bc587ccc12c405190bf4b19d995e
hisami <koron.take@gmail.com>
2020-12-30 17:11:06 +0900

    feat: 初期設定

---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>Hello Woald</h1>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+
+  // メインとなるJavaScriptファイル（エントリーポイント）
+  entry: "./src/index.tsx",
+  devServer: {
+    contentBase: `dist`,
+    port: 3000,
+    open: true,
+    hot: true
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/src/index.html`,
+      hash: true,
+    })
+  ],
+  // ファイルの出力設定
+  output: {
+    //  出力ファイルのディレクトリ名
+    path: `${__dirname}/dist`,
+    // 出力ファイル名
+    filename: "bundle.js"
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        // 拡張子 .ts もしくは .tsx の場合
+        test: /\.tsx?$/,
+        // TypeScript をコンパイルする
+        use: "ts-loader"
+      }
+    ]
+  },
+  // import 文で .ts や .tsx ファイルを解決するため
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  // ES5(IE11等)向けの指定（webpack 5以上で必要）
+  // target: ["web", "es5"],
+};

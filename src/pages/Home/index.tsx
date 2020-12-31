cfb45c939b5190b55fe25292ad97e5d8fa6ef8a8
hisami <koron.take@gmail.com>
2020-12-31 10:00:59 +0900

    feat: material-uiの導入

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { css } from "@emotion/css";
+import Button from "@material-ui/core/Button";
 
 const Home: React.FC = () => {
-  return <div className={styleSample}>This is a Home page.</div>;
+  return (
+    <>
+      <Button variant="contained" color="primary">
+        Button
+      </Button>
+      <div className={styleSample}>This is a Home page.</div>
+    </>
+  );
 };
 
 const styleSample = css({
